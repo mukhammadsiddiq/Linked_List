7917f28354ab7bb2125ff931effcc57d574e7c4d
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-07-29 22:22:27 +0200

    dfs and bfs graphs

--- Graphs/function.py
+++ Graphs/function.py
@@ -1,9 +1,10 @@
 import queue
-import matplotlib.pyplot as ptl
-import networkx
+import time
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
-def dfs_search(graph, start_node):
+def bfs_order(graph, start_node):
     visited = set()
     q = queue.Queue()
     q.put(start_node)
@@ -17,3 +18,41 @@ def dfs_search(graph, start_node):
             for node in graph[vertex]:
                 if node not in visited:
                     q.put(node)
+    return order
+
+
+def dfs_order(graph, start_node, visited=None):
+    if visited is None:
+        visited = set()
+
+    order = []
+    if start_node not in visited:
+        order.append(start_node)
+        visited.add(start_node)
+        for node in graph[start_node]:
+            if node not in visited:
+                order.extend(dfs_order(graph, node, visited))
+    return order
+
+
+def visualize(order, title, g, pos):
+    plt.figure()
+    plt.title(title)
+    for i, node in enumerate(order, start=1):
+        plt.clf()
+        plt.title(title)
+        nx.draw(g, pos, with_labels=True, node_color=['r' if n == node else 'g' for n in g.nodes])
+        plt.draw()
+        plt.pause(0.5)
+    plt.show()
+    time.sleep(0.5)
+
+
+def generate_random(n, m):
+    while True:
+        g = nx.gnm_random_graph(n, m)
+        if nx.is_connected(g):
+            return g
+
+
+
--- Graphs/main.py
+++ Graphs/main.py
@@ -1,2 +1,8 @@
+from function import *
+
+g = generate_random(10, 10)
+pos = nx.spring_layout(g)
+visualize(dfs_order(g, 0), 'dfs_search', g, pos)
+visualize(bfs_order(g, 0), 'bfs_search', g, pos)
 
 
